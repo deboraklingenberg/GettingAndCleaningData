1b549bd38bcee0591bda38c7f049ca1ee8342a8f
deboraklingenberg <deboraklingenberg@gmail.com>
2020-09-28 11:24:16 -0300

    #minor corrections

--- run_analysis.R
+++ run_analysis.R
@@ -63,6 +63,11 @@ names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
 
 #Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
+install.packages("magrittr") 
+install.packages("dplyr") 
+library(magrittr)
+library(dplyr) 
+
 FinalData <- TidyData %>%
   group_by(subject, activity) %>%
   summarise_all(funs(mean))
